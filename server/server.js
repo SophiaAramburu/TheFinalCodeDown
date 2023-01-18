7a9543428ea5fbbd68a46bb81d394f3ddc6e7cf5
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-01-17 20:21:14 -0600

    Added an express route and added some middleware to the server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.listen(5000., () => console.log('server running'));
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const expressGraphQl = require('express-graphql').graphqlHTTP;
 
 app.use('/graphql', expressGraphQl ({
     graphiql: true
 
 }));
+
+//Route to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../barscapes/public/index.html'))
+});
+
+//Need route for homepage
+//Need route for login page
+//Need route for signup page
+//Need route for signed in homepage/profile info
+//Need route for blog page
+//Need route for friend list page
+//Need route for map page
+//Need a route for signed out 
+
+
+
+app.listen(PORT, () =>
+console.log(`Example app listening at http://localhost:${PORT}`)
+);
