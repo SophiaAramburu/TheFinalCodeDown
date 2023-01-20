5ba5ab86e97f39f684e56da4c423e3035037fe39
Tangnay Ou <tangnay.ou@gmail.com>
2023-01-19 18:44:36 -0600

    add the UserPost file in server file, add routes folder for the Post API

--- server/server.js
+++ server/server.js
@@ -1,32 +1,37 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
 //Apollo Server for gql
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const statusRoutes = require("./routes/userPost");
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const expressGraphQl = require('express-graphql').graphqlHTTP;
+const expressGraphQl = require("express-graphql").graphqlHTTP;
 
-app.use('/graphql', expressGraphQl ({
-    graphiql: true
-
-}));
+app.use(
+  "/graphql",
+  expressGraphQl({
+    graphiql: true,
+  })
+);
 
 //Route to index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
-
-
+app.use("/status", statusRoutes);
 
 app.listen(PORT, () =>
-console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Example app listening at http://localhost:${PORT}`)
 );
