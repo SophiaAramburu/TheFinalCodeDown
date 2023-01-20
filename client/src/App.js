ae3e78bece05dd5fd527534ce1951d9ab368bb74
Reggie P <reginaldpitts1@gmail.com>
2023-01-19 21:26:36 -0600

    editing login.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,4 @@ import Signup from './pages/signup';
 import './App.css';
 
 // this is what appears on screen when you run npm start
-function App() {
-  return (
-    <div className="App">
-   <Signup />
-    </div>)}
-    export default App
+
