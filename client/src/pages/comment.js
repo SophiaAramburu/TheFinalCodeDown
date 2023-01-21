793edf4b22d97d96b5ee53654b7d43dc69f0ae96
Reggie P <reginaldpitts1@gmail.com>
2023-01-21 17:44:17 -0600

    finished framework outline for comment.js

--- client/src/pages/comment.js
+++ client/src/pages/comment.js
@@ -1,27 +1,41 @@
-
 import React from 'react'
 import signupPhoto from '../assets/images/signup.png'
 export default function Signup() {
   return (
-
-    <div class="container">
-
     <div class="top-buzz-container">
-      <h3 class= "top-buzz-section-header"> buzzes from last week</h3>
-      
+      <h3 class= "top-buzz-section-header"> Buzzes from last week</h3>
+
+      <br></br>
+
       <div class= "top-buzz">
         <div class= "buzz-contents"></div>
-      <div class= "buzz-text-view"><p class= "buzz-text">I saw Sophia trying to start a fight last night</p>
+      <div class= "buzz-text-view">
+        <p class= "buzz-text">I saw Sophia crying at the bar</p>
+        <p class= "buzz-details-text"> Austin, TX </p></div>
+
+      <br></br>
+
+      <div class= "buzz-text-view">
+        <p class= "buzz-text">I saw Sophia crying at the bar</p>
+      <p class= "buzz-details-text"> Austin, TX </p></div>
+      </div>
+
+      <br></br>
+
+      <div class= "buzz-text-view">
+        <p class= "buzz-text">I saw Sophia crying at the bar</p>
+      <p class= "buzz-details-text"> Austin, TX </p></div>
+
+      <br></br>
+
+      <div class= "buzz-text-view">
+        <p class= "buzz-text">I saw Sophia crying at the bar</p>
       <p class= "buzz-details-text"> Austin, TX </p></div>
       
-      </div>
-      
-    <div class="container ">
-     
-</div>
+      <br></br>
 
+      <button type="button">Create a Buzz!</button>
+
+</div>
   )
 }
-
-// Line 3:8:  'signupPhoto' is defined but never used  no-unused-vars
-
