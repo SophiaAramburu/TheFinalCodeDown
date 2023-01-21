f7b4a26ec90b3613b10b9518f10fc008174bf706
Reggie P <reginaldpitts1@gmail.com>
2023-01-21 17:15:41 -0600

    adding changes

--- client/src/pages/comment.js
+++ client/src/pages/comment.js
@@ -3,8 +3,12 @@ import React from 'react'
 import signupPhoto from '../assets/images/signup.png'
 export default function Signup() {
   return (
-    <div class="container ">
+    <div class="container">
         hello
 </div>
+
   )
 }
+
+// Line 3:8:  'signupPhoto' is defined but never used  no-unused-vars
+
