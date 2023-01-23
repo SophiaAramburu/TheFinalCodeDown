a526df557633c8e13f4da3b58b1c99a6e4dc176e
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-22 22:26:08 -0600

    added folder of componenents

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import Comment from './pages/comment';
 import './App.css';
+import Header from './components/Header';
 
 // this is what appears on screen when you run npm start
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
    <Comment />
+   <Header/>
     </div>)}
     export default App
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Headerlogo from '../assets/images/header.jpg'
+
+const Header = () => {
+    <div>
+        <img src={Headerlogo} alt="logo"/>
+    </div>
+}
+export default Header
