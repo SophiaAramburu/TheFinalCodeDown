58c7f923a27a8ebf9b1fa3384a2031165200bd67
Sophia Aramburu <sophiaconstance12@gmail.com>
2023-01-22 22:06:03 -0800

    added css to comments.js

--- client/src/pages/comment.js
+++ client/src/pages/comment.js
@@ -4,34 +4,34 @@ export default function Signup() {
   return (
     
     <div class="top-buzz-container bg-yellow-200">
-      <h3 class= "top-buzz-section-header bg-yellow-200 text-center font-sans md:font-serif text-5xl tracking-widest"> Buzzes from last week</h3>
+      <h3 class= "top-buzz-section-header bg-yellow-200 text-center font-sans md:font-serif text-6xl tracking-widest animate-pulse"> Latest Buzzes</h3>
 
       <br></br>
 
       <div class= "top-buzz">
         <div class= "buzz-contents"></div>
-      <div class= "buzz-text-view  border-solid border-2 border-indigo-600">
-      <p class= "buzz-text bg-yellow-200 text-center tracking-wide text-white bg-black rounded-md ">I saw Sophia crying at the bar</p><br></br>
-      <p class= "buzz-details-text text-center md:font-serif text-white bg-black" > 📍Austin, TX </p></div><br></br>
+      <div class= "buzz-text-view border-solid border-2 border-indigo-600 space-y-3.5" >
+      <p class= "buzz-text bg-yellow-200 text-center tracking-wide md:font-serif text-white bg-black text-3xl">I saw Sophia crying at the bar</p><br></br>
+      <p class= "buzz-details-text text-center md:font-serif text-white bg-black text-2xl" > 📍Austin, TX </p></div><br></br>
       
       <br></br>
     
       <div class= "buzz-text-view  border-solid border-2 border-indigo-600">
-        <p class= "buzz-text text-center md:font-serif tracking-wide bg-black text-white">I saw Sophia crying at the bar</p> <br></br>
-        <p class= "buzz-details-text text-center md:font-serif text-white bg-black"> 📍Austin, TX </p></div><br></br>
+        <p class= "buzz-text text-center md:font-serif tracking-wide bg-black text-white text-3xl">I saw Sophia crying at the bar</p> <br></br>
+        <p class= "buzz-details-text text-center md:font-serif text-white bg-black text-2xl"> 📍Austin, TX </p></div><br></br>
 
       <br></br>
 
       <div class= "buzz-text-view  border-solid border-2 border-indigo-600">
-        <p class= "buzz-text text-center md:font-serif tracking-wide text-white bg-black">I saw Sophia crying at the bar</p><br></br>
-      <p class= "buzz-details-text text-center md:font-serif text-white bg-black">📍Austin, TX </p></div><br></br>
+        <p class= "buzz-text text-center md:font-serif tracking-wide text-white bg-black text-3xl">I saw Sophia crying at the bar</p><br></br>
+      <p class= "buzz-details-text text-center md:font-serif text-white bg-black text-2xl">📍Austin, TX </p></div><br></br>
       </div>
 
       <br></br>
 
       <div class= "buzz-text-view  border-solid border-2 border-indigo-600">
-        <p class= "buzz-text text-center md:font-serif tracking-wide text-white bg-black">I saw Sophia crying at the bar</p><br></br>
-      <p class= "buzz-details-text text-center md:font-serif text-white bg-black"> 📍Austin, TX </p></div><br></br>
+        <p class= "buzz-text text-center md:font-serif tracking-wide text-white bg-black text-3xl">I saw Sophia crying at the bar</p><br></br>
+      <p class= "buzz-details-text text-center md:font-serif text-white bg-black text-2xl"> 📍Austin, TX </p></div><br></br>
       
       <br></br>
 
