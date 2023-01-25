49ba4b7181c04e18462e561738e735c521c75107
Reggie P <reginaldpitts1@gmail.com>
2023-01-24 20:30:25 -0600

    Redirected buttons to proper files

--- client/src/pages/create-account.js
+++ client/src/pages/create-account.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import signupPhoto from '../assets/images/signup.png'
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   return (
@@ -14,38 +15,48 @@ export default function Signup() {
           <input class ="bg-black text-white"type="text" name="name" placeholder="First Name" />
         </label> 
       </div>
+
       <br></br><br></br>
+
       <div class="input-2 text-2xl md:font-serif">
         <label>
           Last Name:
           <input class ="bg-black text-white" type="text" name="name" placeholder="Last Name" />
         </label>
       </div>
+
       <br></br><br></br>
+
       <div class="input-3 text-2xl md:font-serif">
         <label>
           Email:
           <input class ="bg-black text-white" type="text" name="name" placeholder="Email" />
         </label>
       </div>
+
       <br></br><br></br>
+
       <div class="input-4 text-2xl md:font-serif">
         <label>
           Password:
           <input class ="bg-black text-white" type="text" name="name" placeholder="Password" />
         </label>
       </div>
+
       <br></br><br></br>
+      
       <div class="input-5 text-2xl md:font-serif">
         <label>
           Confirm Password:
           <input class ="bg-black text-white" type="text" name="name" placeholder="Confirm Password" />
         </label>
       </div>
+
       <br></br><br></br>
+
       </form>
 
-      <button class="button text-2xl md:font-serif text-center" type="button">Click me to create an account!</button>
+      <button class="button text-2xl md:font-serif text-center" type="button"><Link to="/pages/comment">Click here to submit!</Link></button>
 
     </div>
   )
