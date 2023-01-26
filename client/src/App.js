4a51b9d8cfdca38b59a43f19d54a9aae90f9e58d
Sophia Aramburu <sophiaconstance12@gmail.com>
2023-01-25 21:24:20 -0800

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,37 @@ import Header from './components/Header.js'
 import Home from './pages/homepage'
 import Login from './pages/login'
 import Signup from './pages/create-account'
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css';
 import { Route, Routes } from "react-router-dom"
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // this is what appears on screen when you run npm start
 function App() {
   return (
-
+    <ApolloProvider client={client}>
     <>
 
     <Header />
@@ -24,7 +47,9 @@ function App() {
     </Routes>
 
   </>
-    
+  </ApolloProvider>
+
+
 
     )}
 
